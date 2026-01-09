8eeab7a74944e9019d0791592c1bf668f9984c1d
Ilham1m4man <ilham141623@gmail.com>
2026-01-09 23:30:11 +0700

    session 2 done

--- app/(landing)/components/layouts/header.tsx
+++ app/(landing)/components/layouts/header.tsx
@@ -1,30 +1,46 @@
+"use client"
+
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
 import { FiSearch, FiShoppingBag } from "react-icons/fi";
+import CartPopup from "../ui/cart-popup";
 
 export default function Header() {
+  const [isCartPopupOpen, setIsCartPopupOpen] = useState(false)
   return (
-    <header className="flex sticky top-0 z-50 bg-primary-light/20 backdrop-blur-2xl justify-around items-center h-20">
-      <Link href={"/"}>
-        <Image
-          src="/images/logo.svg"
-          alt="Logo SportOn"
-          width={127}
-          height={30}
-        />
-      </Link>
-      <nav className="flex gap-20 font-medium">
-        <Link href={"#"} className="relative after:content-[''] after:block after:w-8 after:h-1 after:bg-primary after:rounded-full after:absolute after:-bottom-3 after:left-1/2 after:-translate-1/2">Home</Link>
-        <Link href={"#"}>Category</Link>
-        <Link href={"#"}>Explore Products</Link>
-      </nav>
-      <div className="flex gap-10">
-        <FiSearch size={24} />
-        <div className="relative">
-          <FiShoppingBag size={24} />
-          <div className="absolute rounded-full bg-primary w-3.5 h-3.5 -right-1 -top-1 text-[9px] text-primary-light text-center">
-            3
-          </div>
+    <header>
+      <div className="flex justify-between gap-10 container mx-auto py-7">
+        <Link href="/">
+          <Image
+            src="/images/logo.svg"
+            alt="sporton logo"
+            width={127}
+            height={30}
+          />
+        </Link>
+        <nav className="flex gap-24 font-medium">
+          <Link
+            href="#"
+            className="relative after:content-[''] after:block after:bg-primary after:rounded-full after:h-[3px] after:w-1/2 after:absolute after:left-1/2 after:-translate-x-1/2 after:translate-y-1"
+          >
+            Home
+          </Link>
+          <Link href="#">Category</Link>
+          <Link href="#">Explore Products</Link>
+        </nav>
+        <div className="relative flex gap-10">
+          <FiSearch size={24} />
+          <button
+            className="relative cursor-pointer"
+            onClick={() => setIsCartPopupOpen(!isCartPopupOpen)}
+          >
+            <FiShoppingBag size={24} />
+            <div className="bg-primary rounded-full w-3.5 h-3.5 absolute -top-1 -right-1 text-[10px] text-white text-center">
+              3
+            </div>
+          </button>
+          {isCartPopupOpen && <CartPopup />}
         </div>
       </div>
     </header>
