8f7fbb9edaf9f0b2d69f84a5d5b7099904bb3d22
Ilham1m4man <ilham141623@gmail.com>
2026-03-08 10:23:35 +0700

    fix middleware

--- proxy.ts
+++ proxy.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   const backendUrl = process.env.API_URL_INTERNAL;
 
   if (!backendUrl) {
